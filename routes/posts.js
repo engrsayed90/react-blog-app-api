19ff9bc0d23ef1d1ae5df1a8070068611d6f4c2c
MD SAYDUR RAHMAN <93133412+engrsayed90@users.noreply.github.com>
2023-04-28 22:48:21 +0900

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ const Post = require("../models/Post");
 
 
 //Create new post
-router.post("/", async (req, res) => {
+router.post("https://react-api-vdto.onrender.com/", async (req, res) => {
     const newPost = new Post(req.body);
     try {
       const savedPost = await newPost.save();
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
 
 
 //Update Post
-router.put("/:id", async (req, res) => {
+router.put("https://react-api-vdto.onrender.com/:id", async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
       if (post.username === req.body.username) {
@@ -42,7 +42,7 @@ router.put("/:id", async (req, res) => {
 
   
 //Delete Post
-router.delete("/:id", async (req, res) => {
+router.delete("https://react-api-vdto.onrender.com/:id", async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
       if (post.username === req.body.username) {
@@ -62,7 +62,7 @@ router.delete("/:id", async (req, res) => {
   
 
 //GET Post
-router.get("/:id", async (req, res) => {
+router.get("https://react-api-vdto.onrender.com/:id", async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
       res.status(200).json(post);
@@ -72,7 +72,7 @@ router.get("/:id", async (req, res) => {
   });
 
   //GET ALL POSTS
-router.get("/", async (req, res) => {
+router.get("https://react-api-vdto.onrender.com/", async (req, res) => {
     const username = req.query.user;
     const catName = req.query.cat;
     try {
